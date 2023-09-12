2b6e0315e90e92999699f4694a66c7bbada4ad50
calebv07 <calebv07>
2023-09-12 16:35:58 +0000

    refresh of documents

--- intro.php
+++ intro.php
@@ -4,7 +4,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "DynoMonitorFoodPan374$&%";
-$dbname = "	footballApp";
+$dbname = "footballApp";
 
 //Create a Connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
@@ -13,6 +13,15 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 if(!$conn) {
     die("Connection Broken: " .mysqli_connect_error());
 }
+$sql = "INSERT INTO play (passer, reciever, rusher, returner, tacklerOne, tacklerTwo)
+VALUES (20, 10, 23, 46, 12, 15)";
 
+if (mysqli_query($conn, $sql)) {
+    echo "New Record Created";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
 
 ?>
--- notes.md
+++ notes.md
@@ -4,3 +4,9 @@ PHP can serve items to the client, but mainly it excutes php code on the server
 * Post to that database
 * Using some form of sql language
 When following tutorials, use ones with mysqli for the most up to date version
+
+Operations for SQL
+* Create
+* Read
+* Update
+* Delete
