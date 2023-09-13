5a2b7bc151d28a5ba6dc954c254555a1a73245b8
Aviator2276 <91645065+Aviator2276@users.noreply.github.com>
2023-09-13 13:28:35 -0400

    Finished first php form

--- form.php
+++ form.php
@@ -1,8 +1,28 @@
 <?php
-$firstName = $_POST['firstName'];
-$lastName = $_POST['lastName'];
+$firstName = $_POST["firstName"];
+$lastName = $_POST["lastName"];
+$email = $_POST["email"];
 
+$dbserver = "locahost";
+$dbuser = "root";
+$dbpass = "DynoMonitorFoodPan374$&%";
+$dbname = "practiceForm";
 
+$conn = mysqli_connect($dbserver, $dbuser, $dbpass, $dbname);
+if($conn) {
+    echo "Connection Success!";
+} elseif (!$conn) {
+    die("Connection Broken: " . mysqli_connect_error());
+}
 
+$sql = "INSERT INTO play (firstName, lastName, email)
+VALUES ($firstName, $lastName, $email)";
+
+if (mysqli_query($conn, $sql)) {
+    echo "New Record Created";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+mysqli_close($conn);
 
 ?>
