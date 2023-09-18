4ceaa5449a4f216e0293a92ccc1f409435bc6ea6
calebv07 <calebv07>
2023-09-18 17:20:22 +0000

    added getData.php

---
+++ getData.php
@@ -0,0 +1,37 @@
+<?php
+$dbserver = "localhost";
+$dbuser = "root";
+$dbpass = "DynoMonitorFoodPan374$&%";
+$dbname = "practiceForm";
+
+$conn = mysqli_connect($dbserver, $dbuser, $dbpass, $dbname);
+if($conn) {
+    echo "Connection Success!";
+} elseif (!$conn) {
+    die("Connection Broken: " . mysqli_connect_error());
+}
+
+$sql = "SELECT * FROM identity";
+$result = mysqli_query($conn, $sql);
+
+if(mysqli_num_rows($result)>0) {
+    echo "<table>
+            <tr>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Email</th>
+            <tr>";
+    while($row = mysqli_fetch_assoc($result)) {
+        echo "<tr>
+                <td>" . $row['firstName'] . "</td>
+                <td>" . $row['lastName'] . "</td>
+                <td>" . $row['email'] . "</td>
+              </tr>";
+    }
+    echo "</table>";
+} else {
+    die;
+}
+mysqli_close();
+
+?>
