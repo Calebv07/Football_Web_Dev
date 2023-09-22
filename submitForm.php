24a1140ab830918e9c20a0134ae4eeb80f08cc2b
Calebv07 <98468903+Calebv07@users.noreply.github.com>
2023-09-22 17:15:31 +0000

    Added names to input on index.html

--- submitForm.php
+++ submitForm.php
@@ -1,12 +1,15 @@
 <?php 
 
+// Connection Variables to connect to the database
 $dbserver = 'localhost';
 $dbuser = 'root';
 $dbpass = 'DynoMonitorFoodPan374$&%';
 $dbname = 'footballApp';
 
+// Connection object, statement that connects to the database
 $conn = mysql_connection($dbserver, $dbuser, $dbpass, $dbname); 
 
+// Checking connection, if unable to then kill process.
 if(!$conn) {
   die("Error connecting to database: \n" . mysqli_connect_error());
 }
